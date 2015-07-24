53172582d254c35073f11229f351a969109a7b62
Daniel Noland <daniel.noland@gmail.com>
2015-07-23 23:16:05 -0600

    Decided to make this repo just for the event server.
    Will keep all the web server stuff in the memoryoracle_webui repository.

--- memoryoracle_server/src/js/es5/memoryoracle_server.js
+++
@@ -1,9 +0,0 @@
-System.registerModule("memoryoracle_server/memoryoracle_server.js", [], function() {
-  "use strict";
-  var __moduleName = "memoryoracle_server/memoryoracle_server.js";
-  function f(x) {
-    return x * x;
-  }
-  return {};
-});
-//# sourceURL=memoryoracle_server/memoryoracle_server.js
